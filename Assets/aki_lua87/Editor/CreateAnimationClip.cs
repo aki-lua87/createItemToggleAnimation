45e0c665ae2a67ac1ac8ee50e492159d16fa9dd6
aki_lua87 <aki_lua87@outlook.jp>
2023-06-13 01:07:41 +0900

    コントローラーの作成を追加

--- Assets/aki_lua87/Editor/CreateAnimationClip.cs
+++ Assets/aki_lua87/Editor/CreateAnimationClip.cs
@@ -32,6 +32,23 @@ namespace aki_lua87.AnimationUtils
             offCurve.AddKey(endTime, disableValue);
             offClip.SetCurve(fullPath, typeof(GameObject), "m_IsActive", offCurve);
             UnityEditor.AssetDatabase.CreateAsset(offClip, "Assets/aki_lua87/" + clipName + "_off.anim");
+
+            // AnimatorController を作成
+            var controller = UnityEditor.Animations.AnimatorController.CreateAnimatorControllerAtPath("Assets/aki_lua87/" + clipName + ".controller");
+            UnityEditor.Animations.AnimatorStateMachine stateMachine = controller.layers[0].stateMachine;
+            var onState = stateMachine.AddState("On");
+            var offState = stateMachine.AddState("Off");
+            var onTransition = stateMachine.AddAnyStateTransition(offState);
+            var offTransition = stateMachine.AddAnyStateTransition(onState);
+            controller.AddParameter(clipName, AnimatorControllerParameterType.Bool);
+            offTransition.AddCondition(UnityEditor.Animations.AnimatorConditionMode.If, 0, clipName);
+            onTransition.AddCondition(UnityEditor.Animations.AnimatorConditionMode.IfNot, 0, clipName);
+            onState.motion = onClip;
+            offState.motion = offClip;
+            onTransition.duration = 0.0f;
+            onTransition.canTransitionToSelf = false;
+            offTransition.duration = 0.0f;
+            offTransition.canTransitionToSelf = false;
         }
 
         private static string GetFullPath(Transform t, String delimiter, bool includeRoot = false)
